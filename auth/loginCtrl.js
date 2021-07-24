81d5c130bf17bcf53c58e1f4883472503ea9ad12
sixtux <sixto.jose@gmail.com>
2021-07-24 14:42:14 -0500

    Se aplican cambios para el registro de usuarios

--- auth/loginCtrl.js
+++ auth/loginCtrl.js
@@ -2,6 +2,9 @@ var app = angular.module('loginApp',['ngRoute']);
 app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
 
 	$scope.user = {};        
+    $scope.isCreateUser = false;
+    $scope.user.usertype = 'Client';
+
 
 	// $http.get('http://localhost/servicerest/public/index.php/api/users').success(function(data){
 
@@ -19,6 +22,21 @@ app.controller('loginCtrl', ['$scope','$http', function($scope,$http){
             if (data['status'] === 200){                
                 localStorage.setItem('user', JSON.stringify(data['data']));
                 window.location = "/orders";
+            } else {
+                localStorage.clear();
+                swal("Orders!", data.message, "info");
+            }
+        });
+    }
+
+    $scope.registerUser = function() {
+
+        $http.post('http://localhost/servicerest/public/index.php/api/users/create', $scope.user).success(function(data){
+            
+            if (data['status'] === 201){
+                $scope.user = {};
+                $scope.user.usertype = 'Client';
+                swal('Orders!', data.message, "success");
             } else {
                 localStorage.clear();
                 swal("Orders!", data.message, "info");
